a8f2a8039fecb45c17c0347016c0ec929d0aab79
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-11 20:53:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Valor binario de entrada 1: ");
+            string s1 = Console.ReadLine();
+
+            Console.WriteLine(" Valor binario de entrada 2: ");
+            string s2 = Console.ReadLine();
+
+            Console.WriteLine(" Valor binario de entrada 3: ");
+            string s3 = Console.ReadLine();
+
+            Console.WriteLine(" Valor binario de entrada 4: ");
+            string s4 = Console.ReadLine();
+
+            Console.WriteLine(" Valor binario de entrada 5: ");
+            string s5 = Console.ReadLine();
+
+            // Conversión
+            int b1 = Convert.ToInt32(s1, 2);
+            int b2 = Convert.ToInt32(s2, 2);
+            int b3 = Convert.ToInt32(s3, 2);
+            int b4 = Convert.ToInt32(s4, 2);
+            int b0 = Convert.ToInt32(s5, 2);
+
+            //Resultados
+            Console.WriteLine("Input value 1 in base 10: " + b1);
+            Console.WriteLine("Input value 2 in base 10: " + b2);
+            Console.WriteLine("Input value 3 in base 10: " + b3);
+            Console.WriteLine("Input value 4 in base 10: " + b4);
+            Console.WriteLine("Input value 5 in base 10: " + b0);
+        }
+    }
+}
