520b84146b79d9c69c76b1a022f8998555dad9c3
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-12 16:33:13 -0500

    Add files via upload

---
+++ 1er triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1er_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input del usuario
+            Console.Write("Ingrese b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese Z: ");
+            double z = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            //operaciones matemáticas
+            double c = Math.Atan(z / y);
+            double e = 180 - c;
+            double d = 180 - b - e;
+            double dRad = d * Math.PI / 180;
+            double bRad = b * Math.PI / 180;
+            double w = z / (Math.Sin(dRad));
+            double W = w * w;
+            double Z = z * z;
+
+            double x = (Math.Sqrt(W - Z)) - y;
+
+            Console.WriteLine("x: " + x);
+
+        }
+    }
+}
