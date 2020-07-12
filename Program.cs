06736962bdbd168cf83ea82f152dd967768f6f9a
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-11 21:06:28 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -25,19 +25,18 @@ namespace ConsoleApp11
             Console.WriteLine(" Valor binario de entrada 5: ");
             string s5 = Console.ReadLine();
 
-            // Conversión
+            int b0 = Convert.ToInt32(s5, 2);
             int b1 = Convert.ToInt32(s1, 2);
             int b2 = Convert.ToInt32(s2, 2);
             int b3 = Convert.ToInt32(s3, 2);
             int b4 = Convert.ToInt32(s4, 2);
-            int b0 = Convert.ToInt32(s5, 2);
 
-            //Resultados
+            //valores de cada uno
+            Console.WriteLine("Input value 5 in base 10: " + b0);
             Console.WriteLine("Input value 1 in base 10: " + b1);
             Console.WriteLine("Input value 2 in base 10: " + b2);
             Console.WriteLine("Input value 3 in base 10: " + b3);
             Console.WriteLine("Input value 4 in base 10: " + b4);
-            Console.WriteLine("Input value 5 in base 10: " + b0);
         }
     }
 }
