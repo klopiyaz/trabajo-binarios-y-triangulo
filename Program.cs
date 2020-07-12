7db3eaae86d140191303f49b6ea7370cf908c510
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-11 21:19:28 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,28 +11,28 @@ namespace ConsoleApp11
         static void Main(string[] args)
         {
             Console.WriteLine(" Valor binario de entrada 1: ");
-            string s1 = Console.ReadLine();
+            string binario5 = Console.ReadLine();
 
             Console.WriteLine(" Valor binario de entrada 2: ");
-            string s2 = Console.ReadLine();
+            string binario4 = Console.ReadLine();
 
             Console.WriteLine(" Valor binario de entrada 3: ");
-            string s3 = Console.ReadLine();
+            string binario3 = Console.ReadLine();
 
             Console.WriteLine(" Valor binario de entrada 4: ");
-            string s4 = Console.ReadLine();
+            string binario2 = Console.ReadLine();
 
             Console.WriteLine(" Valor binario de entrada 5: ");
-            string s5 = Console.ReadLine();
+            string binario1 = Console.ReadLine();
 
-            int b0 = Convert.ToInt32(s5, 2);
-            int b1 = Convert.ToInt32(s1, 2);
-            int b2 = Convert.ToInt32(s2, 2);
-            int b3 = Convert.ToInt32(s3, 2);
-            int b4 = Convert.ToInt32(s4, 2);
+            int b0 = Convert.ToInt32(binario1, 2);
+            int b1 = Convert.ToInt32(binario5, 2);
+            int b2 = Convert.ToInt32(binario4, 2);
+            int b3 = Convert.ToInt32(binario3, 2);
+            int b4 = Convert.ToInt32(binario2, 2);
 
             //valores de cada uno
-            Console.WriteLine("Input value 5 in base 10: " + b0);
+            Console.WriteLine("Input value 0 in base 10: " + b0);
             Console.WriteLine("Input value 1 in base 10: " + b1);
             Console.WriteLine("Input value 2 in base 10: " + b2);
             Console.WriteLine("Input value 3 in base 10: " + b3);
