b1143870cf319890165f5ade2d5c232c74b9d5bd
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-12 16:07:32 -0500

    Add files via upload

---
+++ 3er triangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("valor w: ");
+            double w = double.Parse(Console.ReadLine());
+            Console.Write("valor d: ");
+            double d = double.Parse(Console.ReadLine());
+            Console.Write("valor x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            //operaciones matemáticas
+            double dRad = d * Math.PI / 180;
+            double z = w * (Math.Sin(dRad));
+
+            double y = (Math.Sqrt(w*w - z*z)) - x;
+
+            Console.WriteLine("y: " + y);
+
+        }
+    }
+}
