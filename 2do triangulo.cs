a8afe311e274eaf82d7911fd33e32d92a0d7c855
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-12 16:04:38 -0500

    Add files via upload

---
+++ 2do triangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2da_tarea_triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("valor de b: ");
+            double bGrados = double.Parse(Console.ReadLine());
+            Console.WriteLine("valor de d: ");
+            double dGrados = double.Parse(Console.ReadLine());
+            Console.WriteLine("valor y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            double b = bGrados * (Math.PI / 180.0);
+            double d = dGrados * (Math.PI / 180.0);
+
+            double a = Math.PI - (Math.PI / 2) - b - d;
+            double aGrados = a * (Math.PI / 180.0);
+            double z = y / Math.Tan(a);
+
+            Console.WriteLine("z: " + z);
+        }
+    }
+}
